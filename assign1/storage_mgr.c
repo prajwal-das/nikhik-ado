4784660ec410fc7ea24d3c8eae37d3a1fed64338
Prajwal Das <prajwal@pysphere.in>
2023-07-06 18:31:42 -0500

    until writeBlock

--- assign1/storage_mgr.c
+++ assign1/storage_mgr.c
@@ -3,7 +3,7 @@
 #include "storage_mgr.h"
 #include "dberror.h"
 
-static const size_t SIZE = sizeof(char);
+static const size_t BIT_SIZE = sizeof(char);
 
 /*
  * This function starts the storage manager. It prints a message indicating that the storage manager is being initialized.
@@ -12,16 +12,16 @@ void initStorageManager(void) {
     printf("The Storage Manager is being initialized...\n");
 }
 
-int numberOfPages(int size, FILE *openFile) {
-    fseek(openFile, 0, SEEK_END);
-    long records = ftell(openFile);
-    rewind(openFile);
+int numberOfPages(int size, FILE *storageFile) {
+    fseek(storageFile, 0, SEEK_END);
+    long records = ftell(storageFile);
+    rewind(storageFile);
     return records / size;
 }
 
 RC checkFile(char *fileName) {
-    FILE *opFile = fopen(fileName, "r");
-    if (!opFile) {
+    FILE *storageFile = fopen(fileName, "r");
+    if (!storageFile) {
         return RC_FILE_NOT_FOUND;
     }
     return RC_OK;
@@ -29,150 +29,150 @@ RC checkFile(char *fileName) {
 
 /* Creating a Page File */
 RC createPageFile(char *fileName) {
-    FILE *opFile = fopen(fileName, "w");
+    FILE *storageFile = fopen(fileName, "w");
 
-    if (!opFile) {
+    if (!storageFile) {
         return RC_FILE_NOT_FOUND;
     }
 
-    char *memBlk = calloc(PAGE_SIZE, SIZE);
-    fwrite(memBlk, SIZE, PAGE_SIZE, opFile);
-    fclose(opFile);
+    char *memBlk = calloc(PAGE_SIZE, BIT_SIZE);
+    fwrite(memBlk, BIT_SIZE, PAGE_SIZE, storageFile);
+    fclose(storageFile);
     free(memBlk);
     return RC_OK;
 }
 
-RC openPageFile(char *fileName, SM_FileHandle *fHandle) {
-    RC response = checkFile(fileName);
-    if (response != RC_OK) {
-        return response;
+RC openPageFile(char *fileName, SM_FileHandle *fh) {
+    RC validResponse = checkFile(fileName);
+    if (validResponse != RC_OK) {
+        return validResponse;
     }
 
-    FILE *opFile = fopen(fileName, "r");
-    fHandle->totalNumPages = numberOfPages(PAGE_SIZE, opFile);
-    fHandle->mgmtInfo = opFile;
-    fHandle->curPagePos = 0;
-    fHandle->fileName = fileName;
-    return response;
+    FILE *storageFile = fopen(fileName, "r");
+    fh->totalNumPages = numberOfPages(PAGE_SIZE, storageFile);
+    fh->mgmtInfo = storageFile;
+    fh->curPagePos = 0;
+    fh->fileName = fileName;
+    return validResponse;
 }
 
-RC closePageFile(SM_FileHandle *fHandle) {
-    RC response = checkFile(fHandle->fileName);
-    fclose(fHandle->mgmtInfo);
-    return response;
+RC closePageFile(SM_FileHandle *fh) {
+    RC validResponse = checkFile(fh->fileName);
+    fclose(fh->mgmtInfo);
+    return validResponse;
 }
 
 RC destroyPageFile(char *fileName) {
     return remove(fileName) != 0 ? RC_FILE_NOT_FOUND : RC_OK;
 }
 
-RC readBlock(int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
-    RC response = checkFile(fHandle->fileName);
+RC readBlock(int pageNum, SM_FileHandle *fh, SM_PageHandle memPage) {
+    RC validResponse = checkFile(fh->fileName);
 
     if (pageNum < 0) {
         return RC_READ_NON_EXISTING_PAGE;
     }
 
-    FILE *opFile = fopen(fHandle->fileName, "r+");
-    fseek(opFile, pageNum * PAGE_SIZE, SEEK_SET);
-    fread(memPage, SIZE, PAGE_SIZE, opFile);
-    fHandle->curPagePos = pageNum;
-    return response;
+    FILE *storageFile = fopen(fh->fileName, "r+");
+    fseek(storageFile, pageNum * PAGE_SIZE, SEEK_SET);
+    fread(memPage, BIT_SIZE, PAGE_SIZE, storageFile);
+    fh->curPagePos = pageNum;
+    return validResponse;
 }
 
 RC getBlockPos(SM_FileHandle *fh) {
-    RC response = checkFile(fh->fileName);
-    return response != RC_OK ? response : fh->curPagePos;
+    RC validResponse = checkFile(fh->fileName);
+    return validResponse != RC_OK ? validResponse : fh->curPagePos;
 }
 
 
 RC readFirstBlock(SM_FileHandle *fh, SM_PageHandle memPage) {
-    RC response = checkFile(fh->fileName);
+    RC validResponse = checkFile(fh->fileName);
     int pageNum = 0;
-    return response != RC_OK ? response : readBlock(pageNum, fh, memPage);
+    return validResponse != RC_OK ? validResponse : readBlock(pageNum, fh, memPage);
 }
 
 
 RC readPreviousBlock(SM_FileHandle *fh, SM_PageHandle memPage) {
-    RC response = checkFile(fh->fileName);
+    RC validResponse = checkFile(fh->fileName);
     int pageNum = getBlockPos(fh) - 1;
-    return response != RC_OK ? response : readBlock(pageNum, fh, memPage);
+    return validResponse != RC_OK ? validResponse : readBlock(pageNum, fh, memPage);
 }
 
 
 RC readCurrentBlock(SM_FileHandle *fh, SM_PageHandle memPage) {
-    RC response = checkFile(fh->fileName);
+    RC validResponse = checkFile(fh->fileName);
     int pageNum = getBlockPos(fh);
-    return response != RC_OK ? response : readBlock(pageNum, fh, memPage);
+    return validResponse != RC_OK ? validResponse : readBlock(pageNum, fh, memPage);
 }
 
 
 RC readNextBlock(SM_FileHandle *fh, SM_PageHandle memPage) {
-    RC response = checkFile(fh->fileName);
+    RC validResponse = checkFile(fh->fileName);
     int pageNum = fh->curPagePos + 1;
-    return response != RC_OK ? response : readBlock(pageNum, fh, memPage);
+    return validResponse != RC_OK ? validResponse : readBlock(pageNum, fh, memPage);
 }
 
 RC readLastBlock(SM_FileHandle *fh, SM_PageHandle memPage) {
-    RC response = checkFile(fh->fileName);
-    return response != RC_OK ? response : readBlock(
-            numberOfPages(SIZE,
+    RC validResponse = checkFile(fh->fileName);
+    return validResponse != RC_OK ? validResponse : readBlock(
+            numberOfPages(BIT_SIZE,
                           fh->mgmtInfo) - 1, fh, memPage);
 }
 
 
-RC writeBlock(int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
+RC writeBlock(int pageNum, SM_FileHandle *fh, SM_PageHandle memPage) {
 
-    RC response = checkFile(fHandle->fileName);
-    if (response != RC_OK) {
-        return response;
+    RC validResponse = checkFile(fh->fileName);
+    if (validResponse != RC_OK) {
+        return validResponse;
     }
-    if (pageNum < 0 || pageNum > fHandle->totalNumPages) {
+    if (pageNum < 0 || pageNum > fh->totalNumPages) {
         return RC_WRITE_FAILED;
     }
 
-    FILE *opFile = fopen(fHandle->fileName, "r+");
-    fwrite(memPage, SIZE, PAGE_SIZE, opFile);
-    fseek(opFile, 0, SEEK_END);
-    fHandle->totalNumPages = numberOfPages(SIZE, opFile);
-    fHandle->curPagePos = pageNum;
-    return response;
+    FILE *storageFile = fopen(fh->fileName, "r+");
+    fwrite(memPage, BIT_SIZE, PAGE_SIZE, storageFile);
+    fseek(storageFile, 0, SEEK_END);
+    fh->totalNumPages = numberOfPages(BIT_SIZE, storageFile);
+    fh->curPagePos = pageNum;
+    return validResponse;
 }
 
 RC writeCurrentBlock(SM_FileHandle *fh, SM_PageHandle memPage) {
-    RC response = checkFile(fh->fileName);
-    return response != RC_OK ? response : writeBlock(fh->curPagePos, fh, memPage);
+    RC validResponse = checkFile(fh->fileName);
+    return validResponse != RC_OK ? validResponse : writeBlock(fh->curPagePos, fh, memPage);
 }
 
 
-RC appendEmptyBlock(SM_FileHandle *fHandle) {
-    RC response = checkFile(fHandle->fileName);
-    if (response != RC_OK) {
-        return response;
+RC appendEmptyBlock(SM_FileHandle *fh) {
+    RC validResponse = checkFile(fh->fileName);
+    if (validResponse != RC_OK) {
+        return validResponse;
     }
-    char *memBlk = calloc(PAGE_SIZE, SIZE);
+    char *memBlk = calloc(PAGE_SIZE, BIT_SIZE);
 
-    FILE *opFile = fopen(fHandle->fileName, "r+");
-    fwrite(memBlk, 1, PAGE_SIZE, opFile);
-    fHandle->curPagePos = numberOfPages(SIZE, opFile) - 1;
-    fHandle->totalNumPages = numberOfPages(SIZE, opFile);
+    FILE *storageFile = fopen(fh->fileName, "r+");
+    fwrite(memBlk, 1, PAGE_SIZE, storageFile);
+    fh->curPagePos = numberOfPages(BIT_SIZE, storageFile) - 1;
+    fh->totalNumPages = numberOfPages(BIT_SIZE, storageFile);
     free(memBlk);
 
-    return response;
+    return validResponse;
 }
 
-RC ensureCapacity(int numberOfPages, SM_FileHandle *fHandle) {
-    RC response = checkFile(fHandle->fileName);
-    if (response != RC_OK) {
-        return response;
+RC ensureCapacity(int numberOfPages, SM_FileHandle *fh) {
+    RC validResponse = checkFile(fh->fileName);
+    if (validResponse != RC_OK) {
+        return validResponse;
     }
 
-    if (fHandle->totalNumPages > numberOfPages) {
+    if (fh->totalNumPages > numberOfPages) {
         return RC_OK;
     }
 
-    while (fHandle->totalNumPages < numberOfPages) {
-        appendEmptyBlock(fHandle);
+    while (fh->totalNumPages < numberOfPages) {
+        appendEmptyBlock(fh);
     }
-    return response;
+    return validResponse;
 }
